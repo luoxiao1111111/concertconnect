6fcb4bb7b7fc3b328e7661d0d77e66ef362c1d7b
Adrian Griffith <2608000g@student.gla.ac.uk>
2023-03-21 18:39:58 +0000

    update

--- concertconnect/settings.py
+++ concertconnect/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'concertconnect_app',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -134,4 +135,14 @@ MEDIA_URL = '/media/'
 #Login syntax
 
 LOGIN_URL = 'concertconnect:login'
+
+# If True, users can register.
+REGISTRATION_OPEN = True
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login
 
